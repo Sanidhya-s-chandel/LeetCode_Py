3472d5b3689b0d42411552eca3aa861f02e43e85
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-07-18 15:03:33 +0530

    Tenth

--- LeetCode_Sol.py
+++ LeetCode_Sol.py
@@ -524,3 +524,65 @@ class Solution:
             s = s * 10 + x % 10
             x //= 10
         return s == copy
+    
+
+# Q10.) Regular Expression Matching
+
+# Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+# '.' Matches any single character.​​​​
+# '*' Matches zero or more of the preceding element.
+# The matching should cover the entire input string (not partial).
+
+
+# Example 1:
+
+# Input: s = "aa", p = "a"
+# Output: false
+# Explanation: "a" does not match the entire string "aa".
+# Example 2:
+
+# Input: s = "aa", p = "a*"
+# Output: true
+# Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+# Example 3:
+
+# Input: s = "ab", p = ".*"
+# Output: true
+# Explanation: ".*" means "zero or more (*) of any character (.)".
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 20
+# 1 <= p.length <= 20
+# s contains only lowercase English letters.
+# p contains only lowercase English letters, '.', and '*'.
+# It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+
+# Sol_10}
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+
+        def dfs(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if j >= len(p):
+                return i >= len(s)
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            
+            if (j + 1) < len(p) and p[j + 1] == "*":
+                memo[(i, j)] = dfs(i, j + 2) or (match and dfs(i + 1, j))
+                return memo[(i, j)]
+            if match:
+                memo[(i, j)] = dfs(i + 1, j + 1)
+                return memo[(i, j)]
+
+            memo[(i, j)] = False
+            return False
+
+        return dfs(0, 0)
