9752e2293d4a3a995bc2d8011575fb7081990f84
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-04 21:55:55 +0530

    Q 20 Valid Paraentheses

--- LeetCode_Sol.py
+++ LeetCode_Sol.py
@@ -1105,3 +1105,60 @@ class Solution:
         dummy.next = dummy.next.next
 
         return x.next 
+    
+# Q20.) Valid Parentheses
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+ 
+
+# Example 1:
+
+# Input: s = "()"
+
+# Output: true
+
+# Example 2:
+
+# Input: s = "()[]{}"
+
+# Output: true
+
+# Example 3:
+
+# Input: s = "(]"
+
+# Output: false
+
+# Example 4:
+
+# Input: s = "([])"
+
+# Output: true
+
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of parentheses only '()[]{}'.
+
+# Sol_20}
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = [] 
+        for c in s: 
+            if c in '([{':
+                stack.append(c)
+            else:
+                if not stack or \
+                    (c == ')' and stack[-1] != '(') or \
+                    (c == '}' and stack[-1] != '{') or \
+                    (c == ']' and stack[-1] != '['):
+                    return False 
+                stack.pop() 
+        return not stack       
