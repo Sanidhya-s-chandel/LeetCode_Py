4195ddae4c1d184bc3e6dd3d0c378e98a533471d
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-07 22:53:26 +0530

    Q 25

--- LeetCode_Sol.py
+++ LeetCode_Sol.py
@@ -1392,3 +1392,65 @@ class Solution:
             head = first_node.next
     
         return dummy.next   
+
+# 25.) Reverse Nodes in k-Group
+
+# Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+# k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+# You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+# Example 1:
+
+# Input: head = [1,2,3,4,5], k = 2
+# Output: [2,1,4,3,5]
+# Example 2:
+
+# Input: head = [1,2,3,4,5], k = 3
+# Output: [3,2,1,4,5]
+ 
+# Constraints:
+
+# The number of nodes in the list is n.
+# 1 <= k <= n <= 5000
+# 0 <= Node.val <= 1000
+ 
+# Follow-up: Can you solve the problem in O(1) extra memory space?
+
+# Sol_25}
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or k == 1:
+            return head
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+        next_node = dummy
+        prev = dummy
+        
+        count = 0
+        while current.next:
+            current = current.next
+            count += 1
+        
+        while count >= k:
+            current = prev.next
+            next_node = current.next
+            for _ in range(1, k):
+                current.next = next_node.next
+                next_node.next = prev.next
+                prev.next = next_node
+                next_node = current.next
+            prev = current
+            count -= k
+        
+        return dummy.next       
