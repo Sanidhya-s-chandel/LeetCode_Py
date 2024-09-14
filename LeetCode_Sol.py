356f6be0339f4a96f548fb643a7f2d7b53ae2200
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-14 16:57:27 +0530

    Q34

--- LeetCode_Sol.py
+++ LeetCode_Sol.py
@@ -1905,3 +1905,76 @@ class Solution:
                     high = mid - 1
 
         return -1
+
+# Q34.) Find First and Last Position of Element in Sorted Array
+
+# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# Example 2:
+
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+# Example 3:
+
+# Input: nums = [], target = 0
+# Output: [-1,-1]
+
+# Constraints:
+
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# nums is a non-decreasing array.
+# -109 <= target <= 109
+
+# Sol_34}
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findFirstOccurrence(nums: List[int], target: int) -> int:
+            low, high = 0, len(nums) - 1
+            first_occurrence = -1
+            
+            while low <= high:
+                mid = (low + high) // 2
+                
+                if nums[mid] < target:
+                    low = mid + 1
+                elif nums[mid] > target:
+                    high = mid - 1
+                else:
+                    first_occurrence = mid
+                    high = mid - 1 
+            
+            return first_occurrence
+        
+        def findLastOccurrence(nums: List[int], target: int, start: int) -> int:
+            low, high = start, len(nums) - 1
+            last_occurrence = -1
+            
+            while low <= high:
+                mid = (low + high) // 2
+                
+                if nums[mid] < target:
+                    low = mid + 1
+                elif nums[mid] > target:
+                    high = mid - 1
+                else:
+                    last_occurrence = mid
+                    low = mid + 1  
+            
+            return last_occurrence
+
+        start = findFirstOccurrence(nums, target)
+        if start == -1:
+            return [-1, -1]
+        
+        end = findLastOccurrence(nums, target, start)
+        return [start, end]
