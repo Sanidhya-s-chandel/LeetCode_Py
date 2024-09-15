0642fc14b91958fb4fab14ee57b1ce4f42ec7765
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-15 11:31:43 +0530

    Q37

--- Q35 - Q50.py
+++ Q35 - Q50.py
@@ -68,3 +68,84 @@ class Solution:
                 boxes[(r // 3) * 3 + c // 3] |= pos
         
         return True
+    
+# Q37.) Sudoku Solver
+
+# Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+# A sudoku solution must satisfy all of the following rules:
+
+# Each of the digits 1-9 must occur exactly once in each row.
+# Each of the digits 1-9 must occur exactly once in each column.
+# Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+# The '.' character indicates empty cells.
+
+# Example 1:
+
+# Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+# Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+# Explanation: The input board is shown above and the only valid solution is shown below:
+
+# Constraints:
+
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit or '.'.
+# It is guaranteed that the input board has only one solution.
+
+# Sol_37}
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        def canPlace(d, row, col):
+            return not (d in rows[row] or d in cols[col] or d in boxes[(row // 3) * 3 + col // 3])
+
+        def placeNumber(d, row, col):
+            rows[row].add(d)
+            cols[col].add(d)
+            boxes[(row // 3) * 3 + col // 3].add(d)
+            board[row][col] = d
+
+        def removeNumber(d, row, col):
+            rows[row].remove(d)
+            cols[col].remove(d)
+            boxes[(row // 3) * 3 + col // 3].remove(d)
+            board[row][col] = '.'
+
+        def placeNextNumbers(row, col):
+            if row == N - 1 and col == N - 1:
+                nonlocal sudoku_solved
+                sudoku_solved = True
+            elif col == N - 1:
+                backtrack(row + 1, 0)
+            else:
+                backtrack(row, col + 1)
+
+        def backtrack(row=0, col=0):
+            if board[row][col] == '.':
+                for d in range(1, 10):
+                    D = str(d)
+                    if canPlace(D, row, col):
+                        placeNumber(D, row, col)
+                        placeNextNumbers(row, col)
+                        if not sudoku_solved:
+                            removeNumber(D, row, col)
+            else:
+                placeNextNumbers(row, col)
+
+        n = 3
+        N = n * n
+        rows = [set() for _ in range(N)]
+        cols = [set() for _ in range(N)]
+        boxes = [set() for _ in range(N)]
+        sudoku_solved = False
+
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] != '.':
+                    D = board[i][j]
+                    rows[i].add(D)
+                    cols[j].add(D)
+                    boxes[(i // 3) * 3 + j // 3].add(D)
+
+        backtrack()
