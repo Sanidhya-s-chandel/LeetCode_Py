e5d528615054aa1c1a47008effdf8d1cd43f4be0
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-16 22:42:41 +0530

    Q38

--- Q35 - Q50.py
+++ Q35 - Q50.py
@@ -149,3 +149,63 @@ class Solution:
                     boxes[(i // 3) * 3 + j // 3].add(D)
 
         backtrack()
+
+# Q38.) Count and Say
+
+# The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+# countAndSay(1) = "1"
+# countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+# Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
+
+# Given a positive integer n, return the nth element of the count-and-say sequence.
+
+# Example 1:
+
+# Input: n = 4
+
+# Output: "1211"
+
+# Explanation:
+
+# countAndSay(1) = "1"
+# countAndSay(2) = RLE of "1" = "11"
+# countAndSay(3) = RLE of "11" = "21"
+# countAndSay(4) = RLE of "21" = "1211"
+# Example 2:
+
+# Input: n = 1
+
+# Output: "1"
+
+# Explanation:
+
+# This is the base case.
+
+# Constraints:
+
+# 1 <= n <= 30
+
+# Sol_38}
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def count(string):
+            new_string = ''
+            c = 1
+            previous = string[0]
+            for i in range(1, len(string)):
+                if string[i] == previous:
+                    c += 1
+                else:
+                    new_string += str(c) + previous
+                    c = 1
+                    previous = string[i]
+            new_string += str(c) + previous
+            return new_string
+        
+        result = '1'
+        for i in range(1, n):
+            result = count(result)
+        
+        return result
