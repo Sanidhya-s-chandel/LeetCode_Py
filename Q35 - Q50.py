f4816af7f9cdd7c13f020262f50081a2909539ce
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-18 22:24:18 +0530

    Q 40

--- Q35 - Q50.py
+++ Q35 - Q50.py
@@ -257,3 +257,57 @@ class Solution:
                     dp[t].append(comb + [candidate])
         
         return dp[target]
+
+# Q40.) Combination Sum II
+
+# Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+# Each number in candidates may only be used once in the combination.
+
+# Note: The solution set must not contain duplicate combinations.
+
+# Example 1:
+
+# Input: candidates = [10,1,2,7,6,1,5], target = 8
+# Output: 
+# [
+# [1,1,6],
+# [1,2,5],
+# [1,7],
+# [2,6]
+# ]
+# Example 2:
+
+# Input: candidates = [2,5,2,1,2], target = 5
+# Output: 
+# [
+# [1,2,2],
+# [5]
+# ]
+ 
+# Constraints:
+
+# 1 <= candidates.length <= 100
+# 1 <= candidates[i] <= 50
+# 1 <= target <= 30
+
+# Sol_40}
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+
+        def dfs(target, start, comb):
+            if target == 0:
+                res.append(comb)
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] > target:
+                    break  # Prune the branch
+                dfs(target - candidates[i], i + 1, comb + [candidates[i]])
+
+        dfs(target, 0, [])
+        return res
