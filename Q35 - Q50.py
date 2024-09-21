317878d4938f3e78354dad8419571a73da9234b8
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-21 23:19:55 +0530

    Q 46

--- Q35 - Q50.py
+++ Q35 - Q50.py
@@ -537,3 +537,48 @@ class Solution:
                 current_end = farthest
 
         return jumps
+    
+# Q46.) Permutations
+
+# Given an array nums of distinct integers, return all the possible 
+# permutations
+# . You can return the answer in any order.
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# Example 2:
+
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+# Example 3:
+
+# Input: nums = [1]
+# Output: [[1]]
+ 
+# Constraints:
+
+# 1 <= nums.length <= 6
+# -10 <= nums[i] <= 10
+# All the integers of nums are unique.
+
+# Sol_46}
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def heap_permute(n, nums):
+            if n == 1:
+                result.append(nums[:])
+                return
+            
+            for i in range(n):
+                heap_permute(n - 1, nums)
+                if n % 2 == 0:
+                    nums[i], nums[n - 1] = nums[n - 1], nums[i] 
+                else:
+                    nums[0], nums[n - 1] = nums[n - 1], nums[0]  
+
+        result = []
+        heap_permute(len(nums), nums)
+        return result
