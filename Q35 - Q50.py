c94cef0c6367099b187792105ee8706630c84201
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-09-22 21:37:05 +0530

    Q 47

--- Q35 - Q50.py
+++ Q35 - Q50.py
@@ -582,3 +582,50 @@ class Solution:
         result = []
         heap_permute(len(nums), nums)
         return result
+
+# Q47.) Permutations II
+
+# Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+
+# Example 1:
+
+# Input: nums = [1,1,2]
+# Output:
+# [[1,1,2],
+#  [1,2,1],
+#  [2,1,1]]
+# Example 2:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+# Constraints:
+
+# 1 <= nums.length <= 8
+# -10 <= nums[i] <= 10
+
+# Sol_47}
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                if used[i] or (i > 0 and nums[i] == nums[i - 1] and not used[i - 1]):
+                    continue
+                
+                used[i] = True
+                path.append(nums[i])
+                
+                backtrack(path, used)
+                
+                used[i] = False
+                path.pop()
+        
+        nums.sort()  
+        res = []
+        backtrack([], [False] * len(nums))
+        return res
