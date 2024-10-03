1300e0e7250fce7c5aed55e70678684d3445ffcf
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-10-03 23:09:44 +0530

    Q 63

--- Q61 - Q70.py
+++ Q61 - Q70.py
@@ -104,3 +104,52 @@ class Solution:
         
         memo = [[-1 for _ in range(n)] for _ in range(m)]
         return dfs(0, 0)
+
+# Q63.) Unique Paths II
+
+# You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+# An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+
+# Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+# The testcases are generated so that the answer will be less than or equal to 2 * 109.
+
+# Example 1:
+
+# Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+# Output: 2
+# Explanation: There is one obstacle in the middle of the 3x3 grid above.
+# There are two ways to reach the bottom-right corner:
+# 1. Right -> Right -> Down -> Down
+# 2. Down -> Down -> Right -> Right
+# Example 2:
+
+
+# Input: obstacleGrid = [[0,1],[0,0]]
+# Output: 1
+ 
+# Constraints:
+
+# m == obstacleGrid.length
+# n == obstacleGrid[i].length
+# 1 <= m, n <= 100
+# obstacleGrid[i][j] is 0 or 1.
+
+# Sol_63}
+
+class Solution:
+    def uniquePathsWithObstacles(self, A: List[List[int]]) -> int:
+        if A[0][0] or A[-1][-1]:
+            return 0
+        rangeN, rangeM, source = range(len(A)), range(len(A[0])), [(-1, 0), (0, -1)]
+        A[0][0] = -1
+        for i, j, (_i, _j) in product(rangeN, rangeM, source):
+            if A[i][j] == 1:
+                continue
+            try:
+                if i+_i != -1 and j+_j != -1 and A[i+_i][j+_j] != 1:
+                    A[i][j] += A[i+_i][j+_j]
+            except:
+                pass
+        return -A[-1][-1]
