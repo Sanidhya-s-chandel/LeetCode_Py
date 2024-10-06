9541f7c53e98cf9c388c8e2bf22f48b7f57eb0d0
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-10-06 22:26:24 +0530

    Q 69

--- Q61 - Q70.py
+++ Q61 - Q70.py
@@ -417,3 +417,50 @@ class Solution:
             cur += [w]
             num_of_letters += len(w)
         return res + [' '.join(cur).ljust(maxWidth)] 
+
+# Q69.) Sqrt(x)
+
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+# You must not use any built-in exponent function or operator.
+
+# For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+# Example 1:
+
+# Input: x = 4
+# Output: 2
+# Explanation: The square root of 4 is 2, so we return 2.
+# Example 2:
+
+# Input: x = 8
+# Output: 2
+# Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+# Constraints:
+
+# 0 <= x <= 231 - 1
+
+# Sol_69}
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        
+        left, right = 1, x
+        result = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            mid_squared = mid * mid
+            
+            if mid_squared == x:
+                return mid
+            elif mid_squared < x:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return result
