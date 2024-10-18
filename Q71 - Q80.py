f249f4cbe4a9ab69cb17dd5e4624aa3ee4614623
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-10-18 23:01:07 +0530

    Completed

--- Q71 - Q80.py
+++ Q71 - Q80.py
@@ -575,3 +575,18 @@ class Solution:
                 nums[j] = nums[i] 
 
         return j + 1
+
+# OR
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return len(nums)
+
+        j = 1  
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j - 1]: 
+                j += 1 
+                nums[j] = nums[i] 
+
+        return j + 1
