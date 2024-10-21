696a38544f644f845fdab8e0171f2c75a0e473c7
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-10-21 22:47:11 +0530

    Q 83

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -91,3 +91,36 @@ class Solution:
             while nxt is not None and nxt.val == cur.val:
                 nxt = nxt.next
             return self.deleteDuplicates(nxt)
+
+# Q83.) Remove Duplicates from Sorted List
+
+# Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+# Example 1:
+
+# Input: head = [1,1,2]
+# Output: [1,2]
+# Example 2:
+
+# Input: head = [1,1,2,3,3]
+# Output: [1,2,3]
+ 
+# Constraints:
+
+# The number of nodes in the list is in the range [0, 300].
+# -100 <= Node.val <= 100
+# The list is guaranteed to be sorted in ascending order.
+
+# Sol_83}
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = head
+
+        while head and head.next:
+            if head.val == head.next.val:
+                head.next = head.next.next
+            else:
+                head = head.next
+        
+        return res
