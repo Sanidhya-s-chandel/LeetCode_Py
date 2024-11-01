754651455c2754239786779b54da658a34c76e6b
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-11-01 19:25:07 +0530

    Q 95

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -233,3 +233,46 @@ class Solution:
             current = current.right  
 
         return res
+
+# Q95.) Unique Binary Search Trees II
+
+# Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+
+# Example 1:
+
+# Input: n = 3
+# Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+# Example 2:
+
+# Input: n = 1
+# Output: [[1]]
+ 
+# Constraints:
+
+# 1 <= n <= 8
+
+# Sol_95}
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        def solve(left,right,res):
+            if left==right:
+                return [TreeNode(left)]
+            if left>right:
+                return [None]
+            for i in range(left,right+1):
+                for leftn in solve(left,i-1,[]):
+                    for rightn in solve(i+1,right,[]):
+                        node=TreeNode(i)
+                        node.right=rightn
+                        node.left=leftn
+                        res.append(node)
+
+            return res
+        return solve(1,n,[])
