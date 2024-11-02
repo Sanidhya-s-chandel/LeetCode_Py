5ed9c06d52da8d0f92ce79a143155a040e789036
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-11-02 19:47:20 +0530

    Q 96

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -276,3 +276,34 @@ class Solution:
 
             return res
         return solve(1,n,[])
+
+# Q96.) Unique Binary Search Trees
+
+# Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+
+# Example 1:
+
+# Input: n = 3
+# Output: 5
+# Example 2:
+
+# Input: n = 1
+# Output: 1
+ 
+# Constraints:
+
+# 1 <= n <= 19
+
+# Sol_96}
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        @cache
+        def solve(n):
+            if n==1 or n==0:
+                return 1
+            ans=0
+            for i in range(1,n+1):
+                ans+=solve(i-1)*solve(n-i)
+            return ans
+        return solve(n)
