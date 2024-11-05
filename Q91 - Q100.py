9922ab7580f1c99071b5f71da698729d1e08f681
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-11-05 21:52:03 +0530

    Q 99

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -414,3 +414,42 @@ class Solution:
             return valid(node.left, minimum, node.val) and valid(node.right, node.val, maximum)
         
         return valid(root, float("-inf"), float("inf"))
+
+# Q99.) Recover Binary Search Tree
+# You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+# Input: root = [1,3,null,null,2]
+# Output: [3,1,null,null,2]
+# Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+
+# Input: root = [3,1,4,null,null,2]
+# Output: [2,1,4,null,null,3]
+# Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+ 
+# Constraints:
+
+# The number of nodes in the tree is in the range [2, 1000].
+# -231 <= Node.val <= 231 - 1
+ 
+# Follow up: A solution using O(n) space is pretty straight-forward. Could you devise a constant O(1) space solution?
+
+# Sol_99}
+
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        small = big = prev = None
+
+        def inorder(r):
+            nonlocal small, big, prev
+            if not r:
+                return
+            inorder(r.left)
+            if prev and prev.val > r.val:
+                small = r
+                if not big:
+                    big = prev
+            prev = r
+            inorder(r.right)
+
+        inorder(root)
+        small.val, big.val = big.val, small.val
