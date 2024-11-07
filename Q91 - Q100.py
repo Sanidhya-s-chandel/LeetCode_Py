87af6d459f57dade07caf870ec1016257cb0a39d
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-11-07 14:33:02 +0530

    complete

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -499,3 +499,19 @@ class Solution:
             queue.append((node1.right, node2.right))
         
         return True
+
+#  OR
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
