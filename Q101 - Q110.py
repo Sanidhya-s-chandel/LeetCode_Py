0c5636f94560f72cef6ace67bc968b354888dede
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-11-15 22:07:04 +0530

    Q 108

--- Q101 - Q110.py
+++ Q101 - Q110.py
@@ -256,3 +256,54 @@ class Solution:
             return root
         
         return helper(0, len(inorder) - 1)
+
+# Q108.) Convert Sorted Array to Binary Search Tree
+
+# Given an integer array nums where the elements are sorted in ascending order, convert it to a 
+# height-balanced
+#  binary search tree.
+
+# Input: nums = [-10,-3,0,5,9]
+# Output: [0,-3,9,-10,null,5]
+# Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+# Input: nums = [1,3]
+# Output: [3,1]
+# Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ 
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# nums is sorted in a strictly increasing order.
+
+# SOl_108}
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        
+        root = TreeNode(0) 
+        stack = [(root, 0, len(nums) - 1)]
+        
+        while stack:
+            node, left, right = stack.pop()
+            mid = left + (right - left) // 2
+            node.val = nums[mid]
+            
+            if left <= mid - 1:
+                node.left = TreeNode(0) 
+                stack.append((node.left, left, mid - 1))
+                
+            if mid + 1 <= right:
+                node.right = TreeNode(0) 
+                stack.append((node.right, mid + 1, right))
+        
+        return root     
