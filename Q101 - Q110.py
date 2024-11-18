2677fc78df6f3a97ed87b5b112fe892861200806
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-11-18 23:01:44 +0530

    Completed

--- Q101 - Q110.py
+++ Q101 - Q110.py
@@ -439,4 +439,21 @@ class Solution:
             return balanced, height
         
         balanced, _ = checkBalance(root)
-        return balanced       
+        return balanced
+
+# OR
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return (self.Height(root) >= 0)
+    def Height(self, root):
+        if root is None:  return 0
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
+        return max(leftheight, rightheight) + 1        
