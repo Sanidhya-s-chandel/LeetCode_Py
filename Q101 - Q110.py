41cb65279c41d3301ac41e7f83f1d366efbbac10
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-11-28 23:31:52 +0530

    Q 113

--- Q101 - Q110.py
+++ Q101 - Q110.py
@@ -545,3 +545,56 @@ class Solution:
             return targetSum == root.val
         
         return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
+
+# Q113} Path Sum II
+# Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+# A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+# Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+# Output: [[5,4,11,2],[5,8,4,5]]
+# Explanation: There are two paths whose sum equals targetSum:
+# 5 + 4 + 11 + 2 = 22
+# 5 + 8 + 4 + 5 = 22
+
+# Input: root = [1,2,3], targetSum = 5
+# Output: []
+
+# Input: root = [1,2], targetSum = 0
+# Output: []
+ 
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 5000].
+# -1000 <= Node.val <= 1000
+# -1000 <= targetSum <= 1000
+
+# Sol_113}
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        def solve(l,root,ans):
+            if root is None:
+                return
+            l.append(root.val)
+            
+            if root.left is None and root.right is None:
+                if sum(l) == targetSum:
+                    ans.append(l)
+                return
+            x = l[:]
+            if root.left:
+                solve(l,root.left,ans)
+            if root.right:
+                solve(x,root.right,ans)
+
+        ans = []
+        l = []
+        solve(l,root,ans)
+        return ans
