1c2af9ffb871c8cc1e78376a476075340cc1c63e
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-11-29 23:08:01 +0530

    Q 114

--- Q101 - Q115.py
+++ Q101 - Q115.py
@@ -598,3 +598,62 @@ class Solution:
         l = []
         solve(l,root,ans)
         return ans
+
+# Q114.) Flatten Binary Tree to Linked List
+# Given the root of a binary tree, flatten the tree into a "linked list":
+
+# The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+# The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+# Input: root = [1,2,5,3,4,null,6]
+# Output: [1,null,2,null,3,null,4,null,5,null,6]
+
+# Input: root = []
+# Output: []
+
+# Input: root = [0]
+# Output: [0]
+ 
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 2000].
+# -100 <= Node.val <= 100
+ 
+# Follow up: Can you flatten the tree in-place (with O(1) extra space)?
+
+# Sol_114}
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        if root is None:
+            return
+
+        current = root
+        while current is not None:
+
+            if current.left is not None:
+                last = current.left
+                while last.right is not None:
+                    last = last.right
+
+                last.right = current.right
+                current.right = current.left
+                current.left = None
+
+            current = current.right
