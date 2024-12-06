fc726ebbd3e1022465b97fd3dadbd9ec62b65d89
sanidhya-s-chandel <sanidhyasinghchandel31@gmail.com>
2024-12-06 19:49:21 +0530

    Q 116

--- Q101 - Q115.py
+++ Q101 - Q115.py
@@ -708,3 +708,48 @@ class Solution:
                     else:
                         sumlst[idx] += sumlst[idx+1]
         return sumlst[0]
+
+# Q116.} Populating Next Right Pointers in Each Node
+# You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+# struct Node {
+#   int val;
+#   Node *left;
+#   Node *right;
+#   Node *next;
+# }
+# Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+# Initially, all next pointers are set to NULL.
+
+# Input: root = [1,2,3,4,5,6,7]
+# Output: [1,#,2,3,#,4,5,6,7,#]
+# Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+# Input: root = []
+# Output: []
+ 
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 212 - 1].
+# -1000 <= Node.val <= 1000
+ 
+# Follow-up:
+
+# You may only use constant extra space.
+# The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+
+# Sol_116}
+
+class Solution:
+    def connect(self, root):
+        if not root: return None
+        q = deque([root])
+        while q:
+            rightNode = None
+            for _ in range(len(q)):
+                cur = q.popleft()
+                cur.next, rightNode = rightNode, cur
+                if cur.right:
+                    q.extend([cur.right, cur.left])
+        return root
