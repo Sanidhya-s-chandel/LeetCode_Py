90610b791c2a445661c6fe41d0441831b493fbfd
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-02-15 22:09:15 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -654,3 +654,75 @@ class ProductOfNumbers:
 # obj = ProductOfNumbers()
 # obj.add(num)
 # param_2 = obj.getProduct(k)
+
+
+# Q2698.) Find the Punishment Number of an Integer
+# Given a positive integer n, return the punishment number of n.
+
+# The punishment number of n is defined as the sum of the squares of all integers i such that:
+
+# 1 <= i <= n
+# The decimal representation of i * i can be partitioned into contiguous substrings such that the sum of the integer values of these substrings equals i.
+ 
+# Example 1:
+
+# Input: n = 10
+# Output: 182
+# Explanation: There are exactly 3 integers i in the range [1, 10] that satisfy the conditions in the statement:
+# - 1 since 1 * 1 = 1
+# - 9 since 9 * 9 = 81 and 81 can be partitioned into 8 and 1 with a sum equal to 8 + 1 == 9.
+# - 10 since 10 * 10 = 100 and 100 can be partitioned into 10 and 0 with a sum equal to 10 + 0 == 10.
+# Hence, the punishment number of 10 is 1 + 81 + 100 = 182
+# Example 2:
+
+# Input: n = 37
+# Output: 1478
+# Explanation: There are exactly 4 integers i in the range [1, 37] that satisfy the conditions in the statement:
+# - 1 since 1 * 1 = 1. 
+# - 9 since 9 * 9 = 81 and 81 can be partitioned into 8 + 1. 
+# - 10 since 10 * 10 = 100 and 100 can be partitioned into 10 + 0. 
+# - 36 since 36 * 36 = 1296 and 1296 can be partitioned into 1 + 29 + 6.
+# Hence, the punishment number of 37 is 1 + 81 + 100 + 1296 = 1478
+ 
+# Constraints:
+
+# 1 <= n <= 1000
+
+# Sol_2698}
+
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        
+        def partitioned(num):
+
+            string = str(num**2)
+            m = len(string)
+            
+            self.possible = 0
+            def dfs(i, currval, currstring):
+                if self.possible:
+                    return 
+                if i == m:
+                    if (currval is not None):
+                        if len(currstring):
+                            if currval + int(currstring) == num:
+                                self.possible = 1
+                        elif currval == num:
+                            self.possible = 1
+                    return
+                # Do not split
+                dfs(i+1, currval, currstring + string[i])
+                # Split
+                if currval is None:
+                    currval = 0
+                dfs(i+1, currval + int(currstring + string[i]), '')
+            dfs(0, None, '')
+
+        res = 0
+        for i in range(1, n+1):
+            if not (i % 9 == 0 or i % 9 == 1):
+                continue
+            partitioned(i)
+            if self.possible:
+                res += i**2
+        return res
