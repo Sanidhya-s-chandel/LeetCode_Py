46356cbf183ed93d783499e7a80b87cce8a3f9d9
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-02-15 22:10:17 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -726,3 +726,17 @@ class Solution:
             if self.possible:
                 res += i**2
         return res
+
+
+# --------------------------------- OR ---------------------------------------
+
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        def partition(x, target):
+            if x==target: return True
+            if x==0: return target==0
+            for m in (10, 100, 1000):
+                if partition(x//m, target-x%m):
+                    return True
+            return False
+        return sum(x for i in range(1, n+1) if partition(x:=i*i, i))
