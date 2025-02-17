dc58687546c16a71f04a1afd8244f6ce29221b77
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-02-17 22:18:31 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -887,3 +887,9 @@ class Solution:
                     count[place] += 1
         backtrack(0, "")
         return len(result) - 1
+
+# ---------- one line sol. ----------------
+
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        return len(set(p for i in range(1, len(tiles)+1) for p in permutations(tiles, i)))
