050893be804c886daab54a925d1925ecb73134e5
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-02-19 22:08:29 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -607,3 +607,27 @@ class Solution:
                     break
                 cur += partitionSize
         return output
+
+# ============================== OR =====================================
+
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        totalHappy = 3 * (2**(n-1))
+        if k > totalHappy:
+            return ""
+        
+        output = ""
+        choices = "abc"
+        low, high = 1, totalHappy
+        for i in range(n):
+            partitionSize = (high - low + 1) // len(choices)
+            cur = low
+            for c in choices:
+                if k in range(cur, cur + partitionSize):
+                    output += c
+                    low = cur
+                    high = cur + partitionSize - 1
+                    choices = "abc".replace(c, "")
+                    break
+                cur += partitionSize
+        return output
