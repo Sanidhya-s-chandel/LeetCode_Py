6d727b310f6f2c98708d4e8bb2d888902e682c80
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-02-21 22:15:42 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -672,3 +672,19 @@ class Solution(object):
                 res.append('0')
 
         return "".join(res)
+
+class FindElements:
+    def __init__(self, root):
+        self.values = set()
+        self.recover_tree(root, 0)
+
+    def recover_tree(self, node, value):
+        if not node:
+            return
+        self.values.add(value)
+        node.val = value
+        self.recover_tree(node.left, 2 * value + 1)
+        self.recover_tree(node.right, 2 * value + 2)
+
+    def find(self, target):
+        return target in self.values
