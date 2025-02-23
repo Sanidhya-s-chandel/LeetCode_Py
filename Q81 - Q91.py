d1f27490445fd6fc74ce1cac64139bc0e80423e2
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-02-23 19:48:01 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -712,3 +712,27 @@ class Solution:
             return node
 
         return makeTree(0) # start building tree, returns root
+
+
+# ===============================================================================================
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not postorder:
+            return None
+        val = postorder.pop()
+        node = TreeNode(val)
+        if not postorder:
+            return node
+
+        i = postorder.index(preorder[1])
+        node.left = self.constructFromPrePost(preorder[1:i+2], postorder[:i+1])
+        node.right = self.constructFromPrePost(preorder[i+2:], postorder[i+1:])
+        return node
