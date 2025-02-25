ec5abcbcf75d94b7a2680348663b55c621788150
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-02-25 22:16:09 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -799,3 +799,25 @@ class Solution:
 
         
         return int(max_alice_leaf_score)			
+
+
+# ==========================================================================================
+
+class Solution:
+    def numOfSubarrays(self, arr):
+        MOD = 1000000007
+        odd = 0
+        even = 1
+        result = 0
+        sum_ = 0
+
+        for num in arr:
+            sum_ += num
+            if sum_ % 2 == 1:
+                result = (result + even) % MOD
+                odd += 1
+            else:
+                result = (result + odd) % MOD
+                even += 1
+
+        return result
