4ef6e17c241444d577dec5298140789aece259fd
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-03-11 22:18:35 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -1120,3 +1120,18 @@ class Solution:
             l = r
             r+=1
         return res
+# ================================================================================================
+
+class Solution(object):
+    def numberOfSubstrings(self, s):
+        count = [0] * 3  
+        left = 0  
+        result = 0  
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1  
+
+            while count[0] > 0 and count[1] > 0 and count[2] > 0:
+                result += len(s) - i  
+                count[ord(s[left]) - ord('a')] -= 1  
+                left += 1 
+        return result
