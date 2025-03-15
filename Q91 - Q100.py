0a96449f3a8cb0c4b61c600632d08fe9371ab040
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-03-15 21:13:28 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -1198,4 +1198,28 @@ class Solution(object):
 class Solution(object):
     def maximumCandies(self, candies, k):
         return bisect_left(range(1, sum(candies) // k + 1), True, key=lambda c: sum(x // c for x in candies) < k)
-        
+# =====================================================================================================
+
+class Solution(object):
+    def canRob(self, nums, mid, k):
+        count = 0
+        n = len(nums)
+        i = 0
+        while i < n:
+            if nums[i] <= mid:
+                count += 1
+                i += 1
+            i += 1
+        return count >= k
+
+    def minCapability(self, nums, k):
+        left, right = 1, max(nums)
+        ans = right
+        while left <= right:
+            mid = (left + right) // 2
+            if self.canRob(nums, mid, k):
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
