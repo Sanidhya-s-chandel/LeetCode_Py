076f4b270b2aeb725eba10ecc87a3935aeae7918
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-03-25 22:22:50 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -1576,3 +1576,21 @@ class Solution:
             meeting_days_count += current_end - current_start + 1
         
         return days - meeting_days_count
+# ====================================================================================================================
+class Solution:
+  def checkValidCuts(self, n: int, rectangles: list[list[int]]) -> bool:
+    xs = [(startX, endX) for startX, _, endX, _ in rectangles]
+    ys = [(startY, endY) for _, startY, _, endY in rectangles]
+    return max(self._countMerged(xs),
+               self._countMerged(ys)) >= 3
+
+  def _countMerged(self, intervals: list[tuple[int, int]]) -> int:
+    count = 0
+    prevEnd = 0
+    for start, end in sorted(intervals):
+      if start < prevEnd:
+        prevEnd = max(prevEnd, end)
+      else:
+        prevEnd = end
+        count += 1
+    return count
