671f5a878df5bcf8ecb48fe01d5d02a88a43e0fd
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-03-30 23:10:08 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1051,3 +1051,23 @@ class Solution(object):
                         diff_array[right + 1] -= value
             sum_value += diff_array[i]
         return query_count
+# =====================================================================================================================
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_occurrence = {}
+        
+        for i, char in enumerate(s):
+            last_occurrence[char] = i
+        
+        result = []
+        start = 0
+        end = 0
+        
+        for i, char in enumerate(s):
+            end = max(end, last_occurrence[char])
+            
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+        
+        return result
