50e8127d12ffadd9b964c05b061b18650c6463e1
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-03-30 23:11:05 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1071,3 +1071,25 @@ class Solution:
                 start = i + 1
         
         return result
+# ==================================================================================================================
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        hmap = {}
+        n = len(s)
+        result = []
+
+        for i, ele in enumerate(s):
+            hmap[ele] = i
+
+        cur = 0
+        ans = -1
+        for i, ele in enumerate(s):
+            cur += 1
+            if hmap[ele] > ans:
+                ans = hmap[ele]
+
+            if i == ans:
+                result.append(cur)
+                cur = 0
+
+        return result
