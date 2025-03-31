e790cc6fb534214604bf9161c1009fcfb4957715
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-03-31 22:48:41 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1103,3 +1103,13 @@ class Solution(object):
         for i in range(k - 1):
             res += weights[-1 - i] - weights[i]
         return res
+# ================================================================================================================
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        if len(weights) == k or k == 1:
+            return 0
+
+        wt_sum = [weights[i]+weights[i+1] for i in range(len(weights)-1)]
+        wt_sum.sort()
+
+        return sum(wt_sum[-(k-1):]) - sum(wt_sum[:k-1])
