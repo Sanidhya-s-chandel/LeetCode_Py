5f4ded964be263becd64296c235830402ce7207b
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-03-31 22:45:46 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1093,3 +1093,13 @@ class Solution:
                 cur = 0
 
         return result
+# =====================================================================================================================
+class Solution(object):
+    def putMarbles(self, weights, k):
+        n = len(weights) - 1
+        weights = [weights[i] + weights[i + 1] for i in range(n)]
+        weights.sort()
+        res = 0
+        for i in range(k - 1):
+            res += weights[-1 - i] - weights[i]
+        return res
