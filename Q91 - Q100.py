a0335da30265b614f22d3da7740bd86914f862dc
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-02 22:46:10 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -1830,3 +1830,16 @@ class Solution(object):
                     maxTriplet = max(maxTriplet, (nums[i] - nums[j]) * nums[k])
                     j += 1
         return max(0, maxTriplet)
+# =============================================================================================
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        res = 0
+        N = len(nums)
+        left = nums[0]
+        for j in range(1, N):
+            if nums[j] > left:
+                left = nums[j]
+                continue
+            for k in range(j + 1, N):
+                res = max(res, (left - nums[j]) * nums[k])
+        return res
