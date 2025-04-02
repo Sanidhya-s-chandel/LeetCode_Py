b4d262e1135bdcdd6c105276d727dd82a18977c1
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-02 22:45:03 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -1819,3 +1819,14 @@ class Solution:
             else:
                 d[i] = max(d[i+1], A[i][0])
         return d[0]
+# ==============================================================================================
+class Solution(object):
+    def maximumTripletValue(self, nums):
+        maxTriplet = 0
+        for i in range(len(nums)):
+            for k in range(len(nums) - 1, i, -1):
+                j = i + 1
+                while j < k:
+                    maxTriplet = max(maxTriplet, (nums[i] - nums[j]) * nums[k])
+                    j += 1
+        return max(0, maxTriplet)
