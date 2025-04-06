30da39b5b4656169c2f018318cd4e2c5fbddc463
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-06 20:52:02 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1222,3 +1222,27 @@ class Solution(object):
             res.append(nums[i])
             i = prev[i]
         return res
+# =============================================================================================================
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        d = {}
+        nums = sorted(nums)
+        d[1] = [[nums[0]]]
+        max_level = 1
+        for num in nums[1:]:
+            inserted = False
+            for level in range(max_level, 0, -1):
+                for l in d[level]:
+                    if num % l[-1] == 0:
+                        if level + 1 > max_level:
+                            d[level + 1] = [l + [num]]
+                            max_level += 1
+                        else:
+                            d[level + 1] += [l + [num]]
+                        inserted = True
+                        break
+                if inserted:
+                    break
+                if level == 1 and l == d[level][-1]:
+                    d[1] += [[num]]
+        return d[max_level][0]
