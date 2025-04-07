650e8281e26ba42a00d55ede7299ca0ff33fc034
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-08 00:04:51 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1266,3 +1266,16 @@ class Solution:
             possible_sums = next_sums
         
         return target in possible_sums
+# ========================================================================================
+class Solution(object):
+    def canPartition(self, nums):
+        totalSum = sum(nums)
+        if totalSum % 2 != 0:
+            return False
+        targetSum = totalSum // 2
+        dp = [False] * (targetSum + 1)
+        dp[0] = True
+        for num in nums:
+            for currSum in range(targetSum, num - 1, -1):
+                dp[currSum] = dp[currSum] or dp[currSum - num]
+        return dp[targetSum]
