b06df3997eb41a3f77efe39025471db1ccb12f35
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-08 22:06:03 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1279,3 +1279,20 @@ class Solution(object):
             for currSum in range(targetSum, num - 1, -1):
                 dp[currSum] = dp[currSum] or dp[currSum - num]
         return dp[targetSum]
+# =======================================================================================
+
+class Solution:
+    def minimumOperations(self, nums):
+        cnt = 0
+        while True:
+            mpp = {}
+            temp = 0
+            for num in nums:
+                mpp[num] = mpp.get(num, 0) + 1
+                if mpp[num] == 2:
+                    temp += 1
+            if temp == 0:
+                break
+            nums = nums[min(3, len(nums)):]
+            cnt += 1
+        return cnt
