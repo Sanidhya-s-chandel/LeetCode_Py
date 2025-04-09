65af2708ed44f2beb77c4fa3cce4786fdcf6a6bc
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-09 22:47:47 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -1873,3 +1873,20 @@ class Solution:
             if num > prefix_max:
                 prefix_max = num
         return maximum_triplet_value
+# ===================================================================================================
+class Solution(object):
+    def minOperations(self, nums, k):
+        nums.sort()
+        mini = nums[0]
+        if mini < k:
+            return -1
+
+        cnt = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] > k:
+                cnt += 1
+                while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                    i += 1
+            i += 1
+        return cnt
