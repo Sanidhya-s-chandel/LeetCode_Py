4ddc49b38332b92eb9fe469fb50fe2e433380da8
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-16 22:53:28 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -2029,3 +2029,20 @@ class Solution:
                 cnt-=freq[nums[l]]
                 l+=1
         return ans
+# ===========================================================================================================
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        res = 0
+        dic = {}
+        total = 0
+        l = 0
+        for r in range(len(nums)):
+            dic[nums[r]] = dic.get(nums[r], 0) + 1
+            total += dic[nums[r]] - 1
+            res += l
+            while total >= k:
+                res += 1
+                total -= dic[nums[l]] - 1
+                dic[nums[l]] -= 1
+                l += 1
+        return res
