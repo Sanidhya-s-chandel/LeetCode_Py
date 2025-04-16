e0216955ab0bedad143fc9ac2201d42d771aa695
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-16 22:52:31 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -2014,3 +2014,18 @@ class Solution:
                             # 🥋 Rock Lee: "Another success from persistence!" 🥳
         
         return good_triplets
+# ========================================================================================================
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        n=len(nums)
+        freq=defaultdict(int)
+        ans, cnt, l=0, 0, 0
+        for r,x in enumerate(nums):
+            cnt+=freq[x]
+            freq[x]+=1
+            while cnt>=k:
+                ans+=n-r
+                freq[nums[l]]-=1
+                cnt-=freq[nums[l]]
+                l+=1
+        return ans
