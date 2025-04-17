db3d19e2722da0967130b21dd26d9498ff598c6e
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-17 22:43:51 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -2061,3 +2061,20 @@ class Solution:
             index_map[nums[i]].append(i)
 
         return result
+# ==================================================================================================================
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+
+        values = defaultdict(list)
+
+        for i in range(len(nums)):
+            values[nums[i]].append(i)
+
+        res = 0
+        for v, l in values.items():
+            for i in range(len(l)):
+                for j in range(i + 1, len(l)):
+                    if (l[i] * l[j]) % k == 0:
+                        res += 1
+        return res
+        
