468c7d961f694e5fb82b45708ffc3192137c2c32
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-17 22:41:15 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -2046,3 +2046,18 @@ class Solution:
                 dic[nums[l]] -= 1
                 l += 1
         return res
+# ===========================================================================================================
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        index_map = defaultdict(list)   # nums[i] : [i, i2, i3 ...]
+        result = 0
+
+        for i in range(len(nums)):
+            for j in index_map[nums[i]]:
+                if (i * j) % k == 0:
+                    result += 1
+
+            index_map[nums[i]].append(i)
+
+        return result
