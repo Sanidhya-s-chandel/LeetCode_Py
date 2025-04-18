f667ea3780087e7979a78ac4f8a8c4d2248f0b51
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-18 21:50:00 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1554,3 +1554,26 @@ class Solution:
             above = n - nums[i] - 1 - invcounts[nums[i]]
             res += under * above       
         return(res)
+# ==========================================================================================================
+class Solution:
+    h = ["1"]  
+    def countAndSay(self, n: int) -> str:
+        def u(n):
+            if n <= len(Solution.h):  
+                return Solution.h[n - 1]  
+            else:
+                prev = u(n - 1)  
+                result = ""
+                count = 1
+
+                for i in range(1, len(prev)):
+                    if prev[i] == prev[i - 1]:  
+                        count += 1
+                    else:
+                        result += f"{count}{prev[i - 1]}"  
+                        count = 1  
+
+                result += f"{count}{prev[-1]}"
+                Solution.h.append(result)  
+                return result
+        return u(n)
