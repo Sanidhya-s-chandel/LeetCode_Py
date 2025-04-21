7cafd86cb3841da347af8c04d5815a97b13b674e
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-21 22:10:34 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1594,3 +1594,12 @@ class Solution:
             q, r = divmod(v, k+1)
             ans += q*(k+1) + ((k+1) if r > 0 else 0)
         return ans
+# =============================================================================================================
+class Solution(object):
+    def numberOfArrays(self, differences, lower, upper):
+        sum, maxi, mini = 0, 0, 0
+        for x in differences:
+            sum += x
+            maxi = max(maxi, sum)
+            mini = min(mini, sum)
+        return max(0, upper - lower - maxi + mini + 1)
