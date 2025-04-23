7ad1a55f06fa82271c6e206ed8a0a4a267a6c28c
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-23 22:16:16 +0530

    Update Q91 - Q100.py

--- Q91 - Q100.py
+++ Q91 - Q100.py
@@ -2217,3 +2217,24 @@ class Solution:
             bottom_factor += 1
             base <<= 1
         return count
+# ==============================================================================================================
+class Solution(object):
+    def countLargestGroup(self, n):
+        sums = [0] * 37
+        for i in range(1, n + 1):
+            sums[self.digsum(i)] += 1
+
+        maxi, count = 0, 0
+        for i in sums:
+            if i > maxi:
+                maxi, count = i, 1
+            elif i == maxi:
+                count += 1
+        return count
+
+    def digsum(self, n):
+        s = 0
+        while n:
+            s += n % 10
+            n //= 10
+        return s
