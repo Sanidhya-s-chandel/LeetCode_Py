b7098596106ebb0848cf91b98030db7b39ee8d1d
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-24 17:56:40 +0530

    Update Q71 - Q80.py

--- Q71 - Q80.py
+++ Q71 - Q80.py
@@ -590,3 +590,19 @@ class Solution:
                 nums[j] = nums[i] 
 
         return j + 1
+# =================================================================================================
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        distinct = len(set(nums))
+        complete = 0
+        count = collections.Counter()
+        i = 0
+        for j in range(len(nums)):
+            count[nums[j]] += 1
+            while len(count) == distinct:
+                count[nums[i]] -= 1
+                if count[nums[i]] == 0:
+                    del count[nums[i]]
+                i += 1
+            complete += i
+        return complete
