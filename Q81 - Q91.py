02b52a6c529e4ee5f9c76e1572c73a81a8c4f20d
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-27 22:12:14 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1626,3 +1626,12 @@ class Solution(object):
             if 2*(nums[i]+nums[i+2]) == nums[i+1]:
                 count += 1
         return count
+# ==================================================================================================================
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = 0
+        for i in range(1, n - 1):
+            if nums[i] == (nums[i - 1] + nums[i + 1]) * 2:
+                res += 1
+        return res
