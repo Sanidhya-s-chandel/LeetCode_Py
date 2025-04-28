ae3e3510862390423d74400e56211c3c5bde7cb8
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2025-04-28 23:14:29 +0530

    Update Q81 - Q91.py

--- Q81 - Q91.py
+++ Q81 - Q91.py
@@ -1635,3 +1635,19 @@ class Solution:
             if nums[i] == (nums[i - 1] + nums[i + 1]) * 2:
                 res += 1
         return res
+# =====================================================================================================================
+from typing import List
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        left = 0
+        total = 0
+        count = 0
+        for right in range(len(nums)):
+            total += nums[right]
+            while total * (right - left + 1) >= k:
+                total -= nums[left]
+                left += 1
+            count += right - left + 1
+        
+        return count
